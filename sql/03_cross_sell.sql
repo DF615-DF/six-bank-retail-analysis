2bc7aa784a2e3e663140e2f9d2fbac2eb8acf57e
DF615-DF <2965870151@qq.com>
2026-08-03 00:20:58 +0800

    add sql/03_cross_sell.sql

---
+++ sql/03_cross_sell.sql
@@ -0,0 +1,11 @@
+﻿-- 03 交叉销售机会
+  -- 业务问题：成长客户"已有一定资产、但产品覆盖还浅"，是下一轮交叉销售的主要名单。
+  -- 做法：筛选 AUM 20万~80万、产品持有数 <= 2 的客户。
+
+SELECT
+  COUNT(*) AS cross_sell_candidates,
+  ROUND(AVG(aum), 0) AS avg_aum,
+  ROUND(SUM(aum) / 100000000, 2) AS aum_yi
+FROM customers
+WHERE aum BETWEEN 200000 AND 800000
+  AND product_count <= 2;
