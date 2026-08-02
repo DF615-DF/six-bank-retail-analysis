2d289df387759dcc774e4bf8526cbcb65e68de6b
DF615-DF <2965870151@qq.com>
2026-08-03 00:21:00 +0800

    add sql/05_at_risk.sql

---
+++ sql/05_at_risk.sql
@@ -0,0 +1,17 @@
+﻿-- 05 流失预警名单
+-- 业务问题：哪些客户资产不低、但活跃度和交易明显下降？
+-- 做法：用"低活跃 + 低交易 + 有资产"三个条件组合，先筛出可能流失的名单。
+
+SELECT
+  customer_id,
+  city,
+  aum,
+  product_count,
+  monthly_txn_count,
+  active_days
+FROM customers
+WHERE active_days <= 5
+  AND monthly_txn_count <= 2
+  AND aum BETWEEN 20000 AND 800000
+ORDER BY aum DESC
+LIMIT 20;
